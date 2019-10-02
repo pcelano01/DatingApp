1805c28bb12608c75872fe359755de37d00d75e2
pcelano01 <pino.celano@gmail.com>
2019-10-02 15:57:48 +0200

    End of section 4

--- Dating.API/Controllers/ValuesController.cs
+++ Dating.API/Controllers/ValuesController.cs
@@ -24,6 +24,7 @@ namespace Dating.API.Controllers
 
         // GET api/values
         [HttpGet]
+        [AllowAnonymous]
         public async Task<IActionResult>  GetValues()
         {
             var values = await _context.Values.ToListAsync();
---
+++ DatingApp-SPA/src/app/navbar/navbar.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    // console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged successfully!');
+    },
+    error => {
+      console.log('Login failed!');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  loggedOut() {
+    localStorage.removeItem('token');
+    console.log('Logged out!');
+  }
+}
